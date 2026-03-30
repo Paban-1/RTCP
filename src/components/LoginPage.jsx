990fabc230e22e25605995cea8d3b311d2633134
Paban-1 <rpaban89@gmail.com>
2026-03-30 18:02:01 +0530

    Login Page setup

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const LoginPage = () => {
     return (
         <div>
-            This is the login page
+            <form action="">
+                <input type="email" placeholder='Email' />
+                <input type="password" placeholder='Password' />
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
