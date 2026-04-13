48ee155e3068737dc2c2a5205f80b04c34a49c55
Paban-1 <rpaban89@gmail.com>
2026-04-13 17:52:42 +0530

    Setting up Register Page & Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Room from './pages/Room'
 import LoginPage from './pages/LoginPage'
 import PrivateRoutes from './components/PrivateRoutes'
 import { AuthProvider } from './utils/AuthContext'
+import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <AuthProvider>
           <Routes>
             <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
 
             <Route element={<PrivateRoutes />}>
               <Route path='/' element={<Room />} />
